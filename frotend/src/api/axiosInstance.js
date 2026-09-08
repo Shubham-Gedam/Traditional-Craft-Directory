a67d3178a0f0290623b507abea5fd9ae78925a63
Shubham-Gedam <shubhgedam28@gmail.com>
2026-09-08 15:05:42 +0530

    update axios baseURL to include API path

--- frotend/src/api/axiosInstance.js
+++ frotend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://traditional-craft-directory.onrender.com",
+  baseURL: "https://traditional-craft-directory.onrender.com/api",
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
