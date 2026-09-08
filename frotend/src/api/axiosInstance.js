ff5c1b4085a660e7cd4d7322d159859b812710cb
Shubham-Gedam <shubhgedam28@gmail.com>
2026-09-08 14:55:47 +0530

    update axios baseURL to production endpoint

--- frotend/src/api/axiosInstance.js
+++ frotend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://traditional-craft-directory.onrender.com",
   withCredentials: true, 
   headers: {
     "Content-Type": "application/json",
